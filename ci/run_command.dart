54b346c6141494488618090c61a32af65f994fc6
Onea-Radu <radu-andrei.onea@my.fmi.unibuc.ro>
2020-12-08 18:25:30 +0200

    format8

--- ci/run_command.dart
+++ ci/run_command.dart
@@ -1,3 +1,7 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
@@ -7,11 +11,14 @@ import 'package:pedantic/pedantic.dart';
 
 final bool hasColor = stdout.supportsAnsiEscapes;
 
-final Directory rootDir = Directory(Platform.environment['PROJECT_ROOT'] ?? Directory.current.path);
+final Directory rootDir = Directory(
+    Platform.environment['PROJECT_ROOT'] ?? Directory.current.path);
 
 final String bold = hasColor ? '\x1B[1m' : ''; // used for shard titles
 final String red = hasColor ? '\x1B[31m' : ''; // used for errors
-final String green = hasColor ? '\x1B[32m' : ''; // used for section titles, commands
+final String green = hasColor
+    ? '\x1B[32m'
+    : ''; // used for section titles, commands
 final String yellow = hasColor ? '\x1B[33m' : ''; // unused
 final String cyan = hasColor ? '\x1B[36m' : ''; // used for paths
 final String reverse = hasColor ? '\x1B[7m' : ''; // used for clocks
@@ -19,11 +26,11 @@ final String reset = hasColor ? '\x1B[0m' : '';
 final String redLine = '$red━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━$reset';
 
 bool fileFilter(String it) =>
-    it.endsWith('.dart') &&
-    it != '.dart_tool/build/entrypoint/build.dart' &&
-    !it.endsWith('.g.dart') &&
-    !it.endsWith('.test_coverage.dart') &&
-    !it.endsWith('i18n.dart');
+    it.endsWith('.dart') && //
+        it != '.dart_tool/build/entrypoint/build.dart' &&
+        !it.endsWith('.g.dart') &&
+        !it.endsWith('.test_coverage.dart') &&
+        !it.endsWith('i18n.dart');
 
 String get clock {
   final DateTime now = DateTime.now();
@@ -41,7 +48,8 @@ String prettyPrintDuration(Duration duration) {
     result += '${minutes}min ';
   }
   final int seconds = duration.inSeconds - minutes * 60;
-  final int milliseconds = duration.inMilliseconds - (seconds * 1000 + minutes * 60 * 1000);
+  final int milliseconds = duration.inMilliseconds -
+      (seconds * 1000 + minutes * 60 * 1000);
   return result += '$seconds.${milliseconds.toString().padLeft(3, "0")}s';
 }
 
@@ -49,22 +57,23 @@ void printProgress(String action, String workingDir, String command) {
   print('$clock $action: cd $cyan$workingDir$reset; $green$command$reset');
 }
 
-Stream<String> runAndGetStdout(
-  String executable,
-  List<String> arguments, {
-  String workingDirectory,
-  Map<String, String> environment,
-  bool expectNonZeroExit = false,
-  int expectedExitCode,
-  String failureMessage,
-  Function beforeExit,
-}) async* {
-  final String commandDescription = '${path.relative(executable, from: workingDirectory)} ${arguments.join(' ')}';
+Stream<String> runAndGetStdout(String executable,
+    List<String> arguments, {
+      String workingDirectory,
+      Map<String, String> environment,
+      bool expectNonZeroExit = false,
+      int expectedExitCode,
+      String failureMessage,
+      Function beforeExit,
+    }) async* {
+  final String commandDescription = '${path.relative(
+      executable, from: workingDirectory)} ${arguments.join(' ')}';
   final String relativeWorkingDir = path.relative(workingDirectory);
 
   printProgress('RUNNING', relativeWorkingDir, commandDescription);
 
-  final Stopwatch time = Stopwatch()..start();
+  final Stopwatch time = Stopwatch()
+    ..start();
   final Process process = await Process.start(
     executable,
     arguments,
@@ -73,19 +82,25 @@ Stream<String> runAndGetStdout(
   );
 
   unawaited(stderr.addStream(process.stderr));
-  final Stream<String> lines = process.stdout.transform(utf8.decoder).transform(const LineSplitter());
+  final Stream<String> lines = process.stdout.transform(utf8.decoder).transform(
+      const LineSplitter());
   await for (final String line in lines) {
     yield line;
   }
 
   final int exitCode = await process.exitCode;
-  print('$clock ELAPSED TIME: ${prettyPrintDuration(time.elapsed)} for $green$commandDescription$reset in $cyan$relativeWorkingDir$reset');
-  if ((exitCode == 0) == expectNonZeroExit || (expectedExitCode != null && exitCode != expectedExitCode)) {
+  print(
+      '$clock ELAPSED TIME: ${prettyPrintDuration(time
+          .elapsed)} for $green$commandDescription$reset in $cyan$relativeWorkingDir$reset');
+  if ((exitCode == 0) == expectNonZeroExit ||
+      (expectedExitCode != null && exitCode != expectedExitCode)) {
     if (failureMessage != null) {
       print(failureMessage);
     }
     print('$redLine\n'
-        '${bold}ERROR: ${red}Last command exited with $exitCode (expected: ${expectNonZeroExit ? (expectedExitCode ?? 'non-zero') : 'zero'}).$reset\n'
+        '${bold}ERROR: ${red}Last command exited with $exitCode (expected: ${expectNonZeroExit
+        ? (expectedExitCode ?? 'non-zero')
+        : 'zero'}).$reset\n'
         '${bold}Command: $green$commandDescription$reset\n'
         '${bold}Relative working directory: $cyan$relativeWorkingDir$reset\n'
         '$redLine');
@@ -94,25 +109,25 @@ Stream<String> runAndGetStdout(
   }
 }
 
-Future<void> runCommand(
-  String executable,
-  List<String> arguments, {
-  String workingDirectory,
-  Map<String, String> environment,
-  bool expectNonZeroExit = false,
-  int expectedExitCode,
-  String failureMessage,
-  OutputMode outputMode = OutputMode.print,
-  CapturedOutput output,
-  bool skip = false,
-  bool Function(String) removeLine,
-}) async {
+Future<void> runCommand(String executable,
+    List<String> arguments, {
+      String workingDirectory,
+      Map<String, String> environment,
+      bool expectNonZeroExit = false,
+      int expectedExitCode,
+      String failureMessage,
+      OutputMode outputMode = OutputMode.print,
+      CapturedOutput output,
+      bool skip = false,
+      bool Function(String) removeLine,
+    }) async {
   assert(
-      (outputMode == OutputMode.capture) == (output != null),
-      'The output parameter must be non-null with and only with '
+  (outputMode == OutputMode.capture) == (output != null),
+  'The output parameter must be non-null with and only with '
       'OutputMode.capture');
 
-  final String commandDescription = '${path.relative(executable, from: workingDirectory)} ${arguments.join(' ')}';
+  final String commandDescription = '${path.relative(
+      executable, from: workingDirectory)} ${arguments.join(' ')}';
   final String relativeWorkingDir = path.relative(workingDirectory);
   if (skip) {
     printProgress('SKIPPING', relativeWorkingDir, commandDescription);
@@ -120,7 +135,8 @@ Future<void> runCommand(
   }
   printProgress('RUNNING', relativeWorkingDir, commandDescription);
 
-  final Stopwatch time = Stopwatch()..start();
+  final Stopwatch time = Stopwatch()
+    ..start();
   final Process process = await Process.start(
     executable,
     arguments,
@@ -150,7 +166,9 @@ Future<void> runCommand(
   }
 
   final int exitCode = await process.exitCode;
-  print('$clock ELAPSED TIME: ${prettyPrintDuration(time.elapsed)} for $green$commandDescription$reset in $cyan$relativeWorkingDir$reset');
+  print(
+      '$clock ELAPSED TIME: ${prettyPrintDuration(time
+          .elapsed)} for $green$commandDescription$reset in $cyan$relativeWorkingDir$reset');
 
   if (output != null) {
     output
@@ -158,7 +176,8 @@ Future<void> runCommand(
       ..stderr = _flattenToString(await savedStderr);
   }
 
-  if ((exitCode == 0) == expectNonZeroExit || (expectedExitCode != null && exitCode != expectedExitCode)) {
+  if ((exitCode == 0) == expectNonZeroExit ||
+      (expectedExitCode != null && exitCode != expectedExitCode)) {
     if (failureMessage != null) {
       print(failureMessage);
     }
@@ -175,7 +194,9 @@ Future<void> runCommand(
         break;
     }
     print('$redLine\n'
-        '${bold}ERROR: ${red}Last command exited with $exitCode (expected: ${expectNonZeroExit ? (expectedExitCode ?? 'non-zero') : 'zero'}).$reset\n'
+        '${bold}ERROR: ${red}Last command exited with $exitCode (expected: ${expectNonZeroExit
+        ? (expectedExitCode ?? 'non-zero')
+        : 'zero'}).$reset\n'
         '${bold}Command: $green$commandDescription$reset\n'
         '${bold}Relative working directory: $cyan$relativeWorkingDir$reset\n'
         '$redLine');
@@ -183,10 +204,14 @@ Future<void> runCommand(
   }
 }
 
-String _flattenToString(List<List<int>> chunks) => utf8.decode(chunks.expand<int>((List<int> ints) => ints).toList());
+/// Flattens a nested list of UTF-8 code units into a single string.
+String _flattenToString(List<List<int>> chunks) =>
+    utf8.decode(chunks.expand<int>((List<int> ints) => ints).toList());
 
+/// Specifies what to do with command output from [runCommand].
 enum OutputMode { print, capture, discard }
 
+/// Stores command output from [runCommand] when used with [OutputMode.capture].
 class CapturedOutput {
   String stdout;
   String stderr;
